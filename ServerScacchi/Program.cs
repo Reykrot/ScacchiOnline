6c5f6d8560bfe818ad1deabbf95ac799dea6b0a7
Giovanni Morleschi <g.morleschi@vetrya.com>
2019-08-12 09:53:20 +0200

    scacchi online

---
+++ ServerScacchi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerScacchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
